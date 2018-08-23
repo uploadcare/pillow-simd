6dd6ae6433aeeec1c9aee7be7cc3ce3d3bd5e98a
Sergii Tytarenko <s.tytarenko@levi9.com>
2018-08-23 17:49:49 +0300

    Fix comment's value as None in encoderinfo

--- src/PIL/GifImagePlugin.py
+++ src/PIL/GifImagePlugin.py
@@ -524,7 +524,8 @@ def _write_local_header(fp, im, offset, flags):
                  o8(transparency) +       # transparency index
                  o8(0))
 
-    if "comment" in im.encoderinfo and 1 <= len(im.encoderinfo["comment"]) <= 255:
+    if "comment" in im.encoderinfo and im.encoderinfo["comment"] is not None\
+        and 1 <= len(im.encoderinfo["comment"]) <= 255:
         fp.write(b"!" +
                  o8(254) +                # extension intro
                  o8(len(im.encoderinfo["comment"])) +
