7c662f4a3907b17d00c7f6ee0aa0d1395ca57acb
Sergii Tytarenko <s.tytarenko@levi9.com>
2018-08-23 18:15:24 +0300

    Fix empty comment in the method _get_global_header

--- src/PIL/GifImagePlugin.py
+++ src/PIL/GifImagePlugin.py
@@ -692,7 +692,7 @@ def _get_global_header(im, info):
     for extensionKey in ["transparency", "duration", "loop", "comment"]:
         if info and extensionKey in info:
             if ((extensionKey == "duration" and info[extensionKey] == 0) or
-               (extensionKey == "comment" and not (1 <= len(info[extensionKey]) <= 255))):
+               (extensionKey == "comment" and info[extensionKey] is not None and not (1 <= len(info[extensionKey]) <= 255))):
                 continue
             version = b"89a"
             break
